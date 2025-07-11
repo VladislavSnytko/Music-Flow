24d7a796cf6e5173c039a1c48d8f557af4c12bbb
Artem Pyatnistskii <play.1ne@yandex.ru>
2025-07-11 21:59:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import (HTMLResponse)
+from fastapi.responses import (HTMLResponse, JSONResponse)
 from dotenv import load_dotenv
 
 # Импорт роутеров
